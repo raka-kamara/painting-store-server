613857e13aefa85305b62bf3e219a9d0fc5adb0e
raka-kamara <rakakamara@outlook.com>
2024-04-29 18:18:50 +0600

    Server complete

--- index.js
+++ index.js
@@ -26,10 +26,16 @@ async function run() {
     await client.connect();
 
     const paintingCollection = client.db("paintingDB").collection("painting");
-    const userCollection = client.db("paintingDB").collection("user");
+    const categoryCollection = client.db("paintingDB").collection("category");
 
     app.get("/painting", async (req, res) => {
       const cursor = paintingCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/category", async (req, res) => {
+      const cursor = categoryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -45,27 +51,38 @@ async function run() {
 
     // get with id
     app.get("/singleProduct/:id", async (req, res) => {
-      const result = await paintingCollection.findOne({ _id: new ObjectId(req.params.id),
-       });
+      const result = await paintingCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(result);
     });
 
-    app.put("/updateProduct/:id", async(req, res)=>{
-      const query = {_id: new ObjectId(req.params.id)};
-      const data ={
-        $set:{
-          name:req.body.name,
-          price:req.body.price,
-          rating:req.body.rating,
-          customization:req.body.customization,
-          stock:req.body.stock,
-          photo:req.body.photo
-        }
-      }
-      const result = paintingCollection.updateOne(query, data)
-      console.log(result)
+    // get with id category
+    app.get("/singleCategory/:id", async (req, res) => {
+      const result = await categoryCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(result);
-    })
+    });
+
+    app.put("/updateProduct/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const data = {
+        $set: {
+          name: req.body.name,
+          price: req.body.price,
+          rating: req.body.rating,
+          customization: req.body.customization,
+          stock: req.body.stock,
+          photo: req.body.photo,
+        },
+      };
+      const result = paintingCollection.updateOne(query, data);
+      console.log(result);
+      res.send(result);
+    });
+
+    
     // Delete
     app.delete("/painting/:id", async (req, res) => {
       const id = req.params.id;
@@ -78,6 +95,13 @@ async function run() {
       const newPainting = req.body;
       console.log(newPainting);
       const result = await paintingCollection.insertOne(newPainting);
+      res.send(result);
+    });
+
+    app.post("/category", async (req, res) => {
+      const newcategory = req.body;
+      console.log(newcategory);
+      const result = await categoryCollection.insertOne(newcategory);
       res.send(result);
     });
 
